2cd58961f46a813aaffd87aff29c49edd8a8ebd0
ivarcodes <114813138+ivarcodes@users.noreply.github.com>
2024-12-01 22:12:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import router from '../socialmedia/routes/auth.js';
 import databaseConnection  from '../socialmedia/database/db.js'
+import postrouter from '../socialmedia/routes/post.js'
 dotenv.config();
 
 const port = process.env.PORT || 5001;
@@ -9,6 +10,7 @@ const app = express();
 databaseConnection();
 app.use(express.json());
 app.use("/api",router);
+app.use("/api",postrouter)
 
 
 app.listen(port,()=>{
